4772d0ff21f5d1633483a9e1c442b36659c3b448
mahesh patnaik <mahesh.patnaik6g@gmail.com>
2024-07-04 15:36:29 +0530

    local project

---
+++ src/main/java/com/mahesh/JenkinsProjectone/JenkinsProjectoneApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.JenkinsProjectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/JenkinsProjectone/JenkinsProjectoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.JenkinsProjectone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
